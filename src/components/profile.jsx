d2c8883c19aaeeaa1a437c93163fd2a966cffa2b
anggadk01 <kivikdk@gmail.com>
2023-10-03 22:32:20 +0700

    png

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -140,7 +140,7 @@ export default function Profile(){
 
                             <div className='w-full'>
                                 <div className='bg-white h-full justify-center items-center flex rounded-lg overflow-hidden relative p-5 group'>
-                                    <img src="images/Django.png" alt="" />
+                                    <img src="images/django.png" alt="" />
                                     <div className='absolute p-5 backdrop-blur-xl bg-primary w-full h-full flex items-center opacity-0 scale-0 group-hover:scale-100 group-hover:opacity-100 transition ease-linear'>
                                         <div className='text-center w-full text-white'>
                                             <div className='bg-white rounded-full px-5 py-2 inline-block mb-2'>
